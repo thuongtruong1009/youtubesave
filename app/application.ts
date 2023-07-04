5bb44e610bcf40a4faf615aa342d34f7c270059b
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2023-07-04 14:56:56 +0700

    fix: match key format

--- app/application.ts
+++ app/application.ts
@@ -6,15 +6,19 @@ import * as fs from "node:fs";
 import { EnvConf } from "../configs/env";
 import { loggerFormat } from "../configs/logger";
 import routes from "../routes";
+import crypto from "node:crypto";
 
 const server: FastifyInstance<Server, IncomingMessage, ServerResponse> | any =
   fastify({
     http2: true,
     https: {
       allowHTTP1: true,
-      key: fs.readFileSync(path.join(__dirname, "../key.pem"), "utf8"),
-      cert: fs.readFileSync(path.join(__dirname, "../cert.pem")),
-      passphrase: EnvConf.PASSPHRASE,
+      // key: fs.readFileSync(path.join(__dirname, "../private.pem"), "utf8"),
+      // cert: fs.readFileSync(path.join(__dirname, "../public.pem")),
+      // passphrase: EnvConf.PASSPHRASE,
+
+      key: fs.readFileSync(path.join(__dirname, "..", "private.key")),
+      cert: fs.readFileSync(path.join(__dirname, "..", "certificate.crt")),
     },
     logger: loggerFormat[EnvConf.NODE_ENV] ?? true,
   });
